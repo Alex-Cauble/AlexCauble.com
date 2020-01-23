ffb25c99ff7b335415ab9c5de2ed72d5d15ccc7d
alex.cauble <alexander.t.cauble@gmail.com>
2020-01-22 20:36:42 -0600

    added sass file for stye and did some style updates

--- js/main.js
+++ js/main.js
@@ -1,21 +1,38 @@
-const header = document.querySelector('header');
+//Intersection Observer Section
+const header = document.getElementById('head');
 const homeHero = document.querySelector('.home-hero');
 
 const sectionOneOptions = {
   rootMargin: '-50px 0px 0px 0px',
 };
-const sectionOneObserver = new IntersectionObserver(
-  (entries, sectionOneObserver) => {
-    entries.forEach((entry) => {
-      console.log(entry.target);
-      if (!entry.isIntersecting) {
-        header.classList.add('nav-scrolled');
-      } else {
-        header.classList.remove('nav-scrolled');
-      }
-    });
-  },
-  sectionOneOptions,
-);
+const sectionOneObserver = new IntersectionObserver((entries, sectionOneObserver) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add('nav-scrolled');
+    } else {
+      header.classList.remove('nav-scrolled');
+    }
+  });
+}, sectionOneOptions);
 
 sectionOneObserver.observe(homeHero);
+
+//Coffee Counter Section
+const coffeeCounter = document.getElementById('coffee-counter');
+
+function parseDate(str) {
+  let mdy = str.split('/');
+  return new Date(mdy[2], mdy[0] - 1, mdy[1]);
+}
+
+function dateDiff(firstDate, secondDate) {
+  return (secondDate - firstDate) / (1000 * 60 * 60 * 24);
+}
+
+function getCoffeePots() {
+  const coffeesMade = dateDiff(parseDate('20/8/2008'), Date.now());
+  return parseFloat(coffeesMade * 1.5).toFixed(6);
+}
+setInterval(() => {
+  coffeeCounter.innerText = getCoffeePots();
+}, 1000);
