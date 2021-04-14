05d807e2d9b59f44976b388b923d256965e27bfa
Alex Cauble <alexander.t.cauble@gmail.com>
2021-04-13 21:40:16 -0500

    changed the images annd swapped about and project section

--- js/main.js
+++ js/main.js
@@ -1,39 +1,35 @@
 //Intersection Observer Section
-const header = document.getElementById("head");
-const homeHero = document.querySelector(".home-hero");
+const header = document.getElementById('head');
+const homeHero = document.querySelector('.home-hero');
 
 const sectionOneOptions = {
-  rootMargin: "-50px 0px 0px 0px",
+  rootMargin: '-50px 0px 0px 0px',
 };
 const sectionOneObserver = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (!entry.isIntersecting) {
-      header.classList.add("nav-scrolled");
+      header.classList.add('nav-scrolled');
     }
     if (entry.isIntersecting) {
-      header.classList.remove("nav-scrolled");
+      header.classList.remove('nav-scrolled');
     }
   });
 }, sectionOneOptions);
 
 sectionOneObserver.observe(homeHero);
-// Click Down Arrow Section
-const dwnArrowBtn = document.querySelector("#home-down-arrow");
-dwnArrowBtn.addEventListener("click", () => {
-  window.scrollBy({ top: windowHeight, behavior: "smooth" });
-});
+
 // Click Contact Me
-const homeHeroButton = document.querySelector(".btn-home-hero");
-homeHeroButton.addEventListener("click", () => {
+const homeHeroButton = document.querySelector('.btn-home-hero');
+homeHeroButton.addEventListener('click', () => {
   // window.scrollTo(0, document.body.scrollHeight);
-  window.scrollTo({ behavior: "smooth", top: document.body.scrollHeight });
+  window.scrollTo({ behavior: 'smooth', top: document.body.scrollHeight });
 });
 
 //Coffee Counter Section
-const coffeeCounter = document.getElementById("coffee-counter");
+const coffeeCounter = document.getElementById('coffee-counter');
 
 function parseDate(str) {
-  let mdy = str.split("/");
+  let mdy = str.split('/');
   return new Date(mdy[2], mdy[0] - 1, mdy[1]);
 }
 
@@ -42,7 +38,7 @@ function dateDiff(firstDate, secondDate) {
 }
 
 function getCoffeePots() {
-  const coffeesMade = dateDiff(parseDate("20/8/2008"), Date.now());
+  const coffeesMade = dateDiff(parseDate('20/8/2008'), Date.now());
   return parseFloat(coffeesMade * 1).toFixed(6);
 }
 setInterval(() => {
