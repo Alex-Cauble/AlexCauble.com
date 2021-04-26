fc7e1355010d5d69f470c81a6fbdcee32067a39d
Alex Cauble <alexander.t.cauble@gmail.com>
2021-04-25 21:41:28 -0500

    added some work to fix the canvas resize bugs, now working on adding new card for school district websiate

--- js/sketch.js
+++ js/sketch.js
@@ -1,30 +1,35 @@
 // Section Canvas Particles
-let canvas
+let canvas;
 
+let rate = 4;
 let particles = [];
 
-
 function windowResized() {
-  renderCanvas();
+  resizeCanvas(windowWidth, windowHeight);
+  background('rgb(23, 23, 102 )');
+  initParticles();
 }
 
 function renderCanvas() {
   canvas = createCanvas(windowWidth, windowHeight);
-  canvas.position(0, 0);
-  canvas.style('y-index', '0');
-  canvas.style('position', 'absolute');
 
-  let particlesCount = Math.floor(window.innerWidth / 12)
+  initParticles();
+}
+
+function initParticles() {
+  let particlesCount = Math.floor(window.innerWidth / 12);
   particles = [];
 
   for (let i = 0; i < particlesCount; i++) {
     particles.push(new Particle());
   }
+  //
 }
 
 function setup() {
   renderCanvas();
-
+  background('rgb(23, 23, 102 )');
+  frameRate(28);
 }
 
 function draw() {
@@ -32,8 +37,8 @@ function draw() {
   particles.forEach((p, index) => {
     p.update();
     p.draw();
-    p.checkParticles(particles.slice(index))
-  })
+    p.checkParticles(particles.slice(index));
+  });
 }
 
 class Particle {
@@ -41,7 +46,7 @@ class Particle {
     //Position
     this.position = createVector(random(width), random(height));
     //Velocity
-    this.velocity = createVector(random(-2, 2), random(-2, 2));
+    this.velocity = createVector(random(-rate, rate), random(-rate, rate));
     //Size
     this.size = 10;
   }
@@ -52,7 +57,7 @@ class Particle {
 
   draw() {
     noStroke();
-    fill('rgba(255, 255, 255, 0.45)')
+    fill('rgba(255, 255, 255, 0.45)');
     circle(this.position.x, this.position.y, this.size);
   }
 
@@ -66,12 +71,22 @@ class Particle {
   }
 
   checkParticles(particles) {
-    particles.forEach(particle => {
-      const d = dist(this.position.x, this.position.y, particle.position.x, particle.position.y)
+    particles.forEach((particle) => {
+      const d = dist(
+        this.position.x,
+        this.position.y,
+        particle.position.x,
+        particle.position.y,
+      );
       if (d < 120) {
-        stroke('rgba(255,255,255, 0.1)')
-        line(this.position.x, this.position.y, particle.position.x, particle.position.y)
+        stroke('rgba(255,255,255, 0.1)');
+        line(
+          this.position.x,
+          this.position.y,
+          particle.position.x,
+          particle.position.y,
+        );
       }
-    })
+    });
   }
 }
